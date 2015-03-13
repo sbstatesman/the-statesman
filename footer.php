d6bd1d4bdd56573f38c1c642c3b7ba355ec15a12
willcwelch <willcwelch@gmail.com>
2015-03-12 20:29:41 -0400

    updated footer copyright

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 			<footer class="row">
-				<span class="metatext left">&copy; <a href="<?php echo get_page_link(18318); ?>">2014 Statesman Association Inc.</a></span>
+				<span class="metatext left">&copy; <a href="<?php echo get_page_link(18318); ?>">2015 Statesman Association Inc.</a></span>
 				<span class="metatext right"><a href="mailto:editors@sbstatesman.com">Contact Us</a> | <a href="mailto:advertise@sbstatesman.com">Advertise</a> | <a href="mailto:web@sbstatesman.com">Site Feedback</a></span>
 			</footer>
 		</div>

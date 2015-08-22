8a96fb8a07c42b2add423346fed64a2cae7d8e84
willcwelch <willcwelch@gmail.com>
2015-08-22 13:09:33 -0400

    Re-added slick.css to header.

--- header.php
+++ header.php
@@ -45,6 +45,7 @@ Join The Statesman Web & Graphics Section: https://www.facebook.com/groups/14814
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/slick.css"/>
 		<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
 		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 		<!--[if lt IE 9]>

4bc913410a468b30aad672ce340a99004b96cd69
Awakens <akaxd123@gmail.com>
2015-11-06 13:30:05 -0500

    added donate link to footer, directs to a page titled 'Donate'

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 			<footer class="row">
 				<span class="metatext left">&copy; <a href="<?php echo get_permalink( get_page_by_title( 'About' ) ); ?>">2015 Statesman Association Inc.</a></span>
-				<span class="metatext right"><a href="mailto:editors@sbstatesman.com">Contact Us</a> | <a href="mailto:advertise@sbstatesman.com">Advertise</a> | <a href="mailto:web@sbstatesman.com">Site Feedback</a></span>
+				<span class="metatext right"><a href="mailto:editors@sbstatesman.com">Contact Us</a> | <a href="<?php echo get_permalink( get_page_by_title( 'Donate' ) ); ?>">Donate</a> | <a href="mailto:advertise@sbstatesman.com">Advertise</a> | <a href="mailto:web@sbstatesman.com">Site Feedback</a></span>
 			</footer>
 		</div>
     <?php wp_footer(); ?>

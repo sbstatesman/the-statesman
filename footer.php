962993e3741762e1447f5f531d8e3f047d6058dd
willcwelch <willcwelch@gmail.com>
2016-02-21 19:34:54 -0500

    Added '@' to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 			<footer class="row">
-				<span class="metatext left">&#9786; <a href="<?php echo get_permalink( get_page_by_title( 'About' ) ); ?>">2015 Statesman Association Inc.</a></span>
+				<span class="metatext left">&#64; <a href="<?php echo get_permalink( get_page_by_title( 'About' ) ); ?>">2015 Statesman Association Inc.</a></span>
 				<span class="metatext right"><a href="mailto:editors@sbstatesman.com">Contact Us</a> | <a href="<?php echo get_permalink( get_page_by_title( 'Donate' ) ); ?>">Donate</a> | <a href="mailto:advertise@sbstatesman.com">Advertise</a> | <a href="mailto:web@sbstatesman.com">Site Feedback</a></span>
 			</footer>
 		</div>

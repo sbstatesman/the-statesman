f706581fbba4d0e363e7c4cce1668422d8ee7708
Awakens <akaxd123@gmail.com>
2016-03-15 20:40:53 -0400

    Edit category-news html
    Swap the first two columns

--- category-news.php
+++ category-news.php
@@ -55,27 +55,6 @@
 			</article>
 		</sidebar>
 		<main class="main vline-medium">
-			<div class="main-threeeigth vline-medium">
-				<?php $args = array( 'posts_per_page' => 3, 'offset' => 1, 'category__and' => array($top_story, $news), 'tag__not_in' => array(get_tag_id('campus-briefing'), get_tag_id('under-the-microscope'))); ?>
-				<?php $myposts = new WP_Query( $args ); ?>
-				<?php if ( $myposts->have_posts() ) : ?>
-				<?php while ( $myposts->have_posts() ) : ?>
-				<?php $myposts->the_post(); ?>
-				<article class="vmedia">
-					<figure class="thumbnail">
-						<?php if ( has_post_thumbnail()) {the_post_thumbnail('medium');} ?>
-					</figure>
-					<div class="block">
-						<h3 id="post-<?php the_ID(); ?>">
-							<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-						</h3>
-						<p class="metatext metatext-byline small-text">By <?php the_author_posts_link(); ?> / <a href="<?php the_archive_date(); ?>"><?php the_time('F j, Y'); ?></a></p>
-						<p class="excerpt"><?php get_excerpt(); ?></p>
-					</div>
-				</article>
-				<?php endwhile; ?>
-				<?php endif; ?>
-			</div>
 			<div class="main-fiveeigth">
 				<?php $args = array( 'posts_per_page' => 3, 'category__and' => array($top_story, $news), 'tag__not_in' => array(get_tag_id('campus-briefing'), get_tag_id('under-the-microscope'))); ?>
 				<?php $myposts = new WP_Query( $args ); ?>
@@ -110,6 +89,27 @@
 				<?php endwhile; ?>
 				<?php endif; ?>
 			</div>
+			<div class="main-threeeigth vline-medium">
+				<?php $args = array( 'posts_per_page' => 3, 'offset' => 1, 'category__and' => array($top_story, $news), 'tag__not_in' => array(get_tag_id('campus-briefing'), get_tag_id('under-the-microscope'))); ?>
+				<?php $myposts = new WP_Query( $args ); ?>
+				<?php if ( $myposts->have_posts() ) : ?>
+				<?php while ( $myposts->have_posts() ) : ?>
+				<?php $myposts->the_post(); ?>
+				<article class="vmedia">
+					<figure class="thumbnail">
+						<?php if ( has_post_thumbnail()) {the_post_thumbnail('medium');} ?>
+					</figure>
+					<div class="block">
+						<h3 id="post-<?php the_ID(); ?>">
+							<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+						</h3>
+						<p class="metatext metatext-byline small-text">By <?php the_author_posts_link(); ?> / <a href="<?php the_archive_date(); ?>"><?php the_time('F j, Y'); ?></a></p>
+						<p class="excerpt"><?php get_excerpt(); ?></p>
+					</div>
+				</article>
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>			
 		</main>
 	</section>
 	<div class="hline hline-medium"></div>
